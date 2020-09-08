12ba1dd28ca7a51b9d56c3d3b1f925213f8ed787
Corey Tollerud <tollerudsoftware@gmail.com>
2020-09-08 19:09:01 +0200

    Created minimum compileable product. The remainder of the primitives will follow next.

---
+++ src/FluentDiagrams/Primitives/Coordinate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentDiagrams.Primitives
+{
+	public class Coordinate
+	{
+		public Coordinate( decimal x, decimal y )
+		{
+			X = x;
+			Y = y;
+		}
+
+		/// <summary>
+		/// Get the cartesian coordinate X position
+		/// </summary>
+		public decimal X { get; }
+
+		/// <summary>
+		/// Get the cartesian coordinate Y position
+		/// </summary>
+		public decimal Y { get; }
+	}
+}
